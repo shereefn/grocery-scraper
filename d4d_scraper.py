2211bfd291dd59dafcb0e67c0c0a7f304cbdab9c
shereefn <37159220+shereefn@users.noreply.github.com>
2026-05-02 15:51:58 +0300

    Update d4d_scraper.py

--- d4d_scraper.py
+++ d4d_scraper.py
@@ -26,7 +26,7 @@ SUPABASE_KEY   = os.environ.get("SUPABASE_KEY")
 SEARCH_URL    = "https://d4donline.com/en/saudi-arabia/riyadh/products"
 CARD_SELECTOR = "a.product-card"
 
-TEST_STORES = ["Danube"] 
+TEST_STORES = ["LULU Hypermarket", "Hyper Panda"]
 TARGET_PRODUCTS = []
 
 OUTPUT_HTML = Path("d4d_results.html")
