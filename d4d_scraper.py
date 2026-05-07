833eba51030392a9e2f027b135d294f222d181a3
shereefn <37159220+shereefn@users.noreply.github.com>
2026-05-07 08:53:45 +0300

    Update d4d_scraper.py

--- d4d_scraper.py
+++ d4d_scraper.py
@@ -49,11 +49,7 @@ PRICE_ALERTS = [
 SEARCH_URL    = "https://d4donline.com/en/saudi-arabia/riyadh/products"
 CARD_SELECTOR = "a.product-card"
 
-TEST_STORES = [
-    "LULU Hypermarket", "Hyper Panda", "Othaim Markets", "Nesto", 
-    "eXtra", "Danube", "Mark & Save", "Grand Hyper", 
-    "Hyper Al Wafa", "Al Madina Hypermarket", "Jarir Bookstore"
-]
+TEST_STORES = ["NESTO"]
 TARGET_PRODUCTS = []
 
 OUTPUT_HTML = Path("d4d_results.html")
