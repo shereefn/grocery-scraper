413c50e10fd0b432003e60e949dee030420e8b11
Shereef Neikkan <37159220+shereefn@users.noreply.github.com>
2026-06-09 14:45:44 +0300

    Update d4d_scraper.py

--- d4d_scraper.py
+++ d4d_scraper.py
@@ -354,7 +354,7 @@ async def read_product_name_from_image(image_url: str, http_client: httpx.AsyncC
             )
 
             response = await client.aio.models.generate_content(
-                model='gemini-1.5-flash',
+                model='gemini-2.0-flash',
                 contents=[
                     types.Part.from_bytes(data=resp.content, mime_type='image/jpeg'),
                     prompt
